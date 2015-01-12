79ac243f936a16d8dfb8e738be25d4652f6ed0a3
Jypotheren734x <komarnickolas@gmail.com>
2015-01-12 09:31:33 -0700

    16

--- src/org/usfirst/frc/team5071/robot/Robot.java
+++ src/org/usfirst/frc/team5071/robot/Robot.java
@@ -26,7 +26,7 @@ public class Robot extends IterativeRobot {
 	private boolean AButton, BButton, XButton, YButton, RightBumper,
 			LeftBumper;
 	private double axisXleft, axisYleft, axisXright, axisYright, Trigger;
-	public Talon leftMotor, rightMotor;
+	private Talon leftMotor, rightMotor;
 
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -85,20 +85,14 @@ public class Robot extends IterativeRobot {
 		AButton = xbox.getRawButton(1);
 		BButton = xbox.getRawButton(2);
 		XButton = xbox.getRawButton(3);
-		YButton = xbox.getRawButton(4);
-		while (true) {
-			axisXleft = xbox.getRawAxis(1);
-			axisYleft = xbox.getRawAxis(2);
-			Trigger = xbox.getRawAxis(3);
-			axisXright = xbox.getRawAxis(4);
-			axisYright = xbox.getRawAxis(5);
-			leftMotor.set(axisXleft - axisYleft);
-			rightMotor.set(axisYleft - axisXleft);
-		}
-		/*
-		 * if (button == true) { talon.set(.5); } else if (button == false) {
-		 * talon.set(0); }
-		 */
+		axisXleft = xbox.getRawAxis(1);
+		axisYleft = xbox.getRawAxis(2);
+		Trigger = xbox.getRawAxis(3);
+		axisXright = xbox.getRawAxis(4);
+		axisYright = xbox.getRawAxis(5);
+		leftMotor.set(axisXleft - axisYleft);
+		rightMotor.set(axisYleft - axisXleft);
+
 	}
 
 	/**
@@ -106,5 +100,135 @@ public class Robot extends IterativeRobot {
 	 */
 	public void testPeriodic() {
 		LiveWindow.run();
+	}
+	
+	
+
+	public RobotDrive getRobit() {
+		return robit;
+	}
+
+	public void setRobit(RobotDrive robit) {
+		this.robit = robit;
+	}
+
+	public Joystick getXbox() {
+		return xbox;
+	}
+
+	public void setXbox(Joystick xbox) {
+		this.xbox = xbox;
+	}
+
+	public Command getAutonomousCommand() {
+		return autonomousCommand;
+	}
+
+	public void setAutonomousCommand(Command autonomousCommand) {
+		this.autonomousCommand = autonomousCommand;
+	}
+
+	public boolean isAButton() {
+		return AButton;
+	}
+
+	public void setAButton(boolean aButton) {
+		AButton = aButton;
+	}
+
+	public boolean isBButton() {
+		return BButton;
+	}
+
+	public void setBButton(boolean bButton) {
+		BButton = bButton;
+	}
+
+	public boolean isXButton() {
+		return XButton;
+	}
+
+	public void setXButton(boolean xButton) {
+		XButton = xButton;
+	}
+
+	public boolean isYButton() {
+		return YButton;
+	}
+
+	public void setYButton(boolean yButton) {
+		YButton = yButton;
+	}
+
+	public boolean isRightBumper() {
+		return RightBumper;
+	}
+
+	public void setRightBumper(boolean rightBumper) {
+		RightBumper = rightBumper;
+	}
+
+	public boolean isLeftBumper() {
+		return LeftBumper;
+	}
+
+	public void setLeftBumper(boolean leftBumper) {
+		LeftBumper = leftBumper;
+	}
+
+	public double getAxisXleft() {
+		return axisXleft;
+	}
+
+	public void setAxisXleft(double axisXleft) {
+		this.axisXleft = axisXleft;
+	}
+
+	public double getAxisYleft() {
+		return axisYleft;
+	}
+
+	public void setAxisYleft(double axisYleft) {
+		this.axisYleft = axisYleft;
+	}
+
+	public double getAxisXright() {
+		return axisXright;
+	}
+
+	public void setAxisXright(double axisXright) {
+		this.axisXright = axisXright;
+	}
+
+	public double getAxisYright() {
+		return axisYright;
+	}
+
+	public void setAxisYright(double axisYright) {
+		this.axisYright = axisYright;
+	}
+
+	public double getTrigger() {
+		return Trigger;
+	}
+
+	public void setTrigger(double trigger) {
+		Trigger = trigger;
+	}
+
+	public Talon getLeftMotor() {
+		return leftMotor;
+	}
+
+	public void setLeftMotor(Talon leftMotor) {
+		this.leftMotor = leftMotor;
+	}
+
+	public Talon getRightMotor() {
+		return rightMotor;
+	}
+
+	public void setRightMotor(Talon rightMotor) {
+		this.rightMotor = rightMotor;
 	}
 }
